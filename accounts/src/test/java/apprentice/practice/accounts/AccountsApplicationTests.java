07f7d80c97aecd550e850ca5f12313b6874e7a63
Shiyi.GAO <siygao@thoughtworks.com>
2019-12-11 15:07:10 +0800

    [Shiyi] add accounts service

---
+++ accounts/src/test/java/apprentice/practice/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package apprentice.practice.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
