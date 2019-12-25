a8b15aa718ff5566e4f482caddc5858c8dd53ed4
Shiyi.GAO <siygao@thoughtworks.com>
2019-12-25 10:30:19 +0800

    [Shiyi] remove enable transaction from accouts service

--- accounts/src/main/java/apprentice/practice/accounts/AccountsApplication.java
+++ accounts/src/main/java/apprentice/practice/accounts/AccountsApplication.java
@@ -2,10 +2,8 @@ package apprentice.practice.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class AccountsApplication {
 
   public static void main(String[] args) {
